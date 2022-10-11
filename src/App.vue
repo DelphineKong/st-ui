81d275fdfb361b92dcfb2ced6da6cf3b5fd739f4
搬砖的女娃子 <1239457176@qq.com>
2022-10-11 10:33:19 +0800

    搭建html页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-  <div>导航栏|<router-link to="/">HelloWorld</router-link>|<router-link to="/hhh">HelloWorld2</router-link></div>
-  <hr/>
   <router-view></router-view>
 </template>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>st-ui</h1>
+            <h2>前端ui框架</h2>
+            <p class="actions">
+                <a href="">GitHub</a>
+                <a href="">开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.topnav{
+    border: 1px solid red;
+}
+</style>
