6cf83d84eb74f4211cc57a280c48fb8a5c999e47
搬砖的女娃子 <1239457176@qq.com>
2022-10-12 16:02:31 +0800

    优化整理css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,60 +1,66 @@
 <template>
-    <div>
-        <button :class="{checked:value}" @click="toggle"><span></span></button>
-        <div>{{value}}</div>
-    </div>
+  <div>
+    <button :class="{ checked: value }" @click="toggle"><span></span></button>
+    <div>{{ value }}</div>
+  </div>
 </template>
 <script lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
-  props:{
-    value:Boolean
+  props: {
+    value: Boolean,
   },
-    setup(props,context){
-        const toggle=()=>{
-          context.emit('update:value',!props.value)
-          // this.$emit()
-        }
-        return{toggle}
-
-    }
-}
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+      // this.$emit()
+    };
+    return { toggle };
+  },
+};
 </script>
 <style lang="scss" scoped>
-  $h: 22px;
-  $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: #bfbfbf;
-    border-radius: $h/2;
-    position: relative;
-  }
-  span{
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
+  > span {
     position: absolute;
     top: 2px;
     left: 2px;
     height: $h2;
     width: $h2;
-    background:white;
+    background: white;
     border-radius: $h2 / 2;
     transition: all 250ms;
   }
-  button.checked{
+  &.checked {
     background: #1890ff;
-  }
-  button.checked > span {
+    > span {
     left: calc(100% - #{$h2} - 2px);
+    }
   }
-  button:focus{
+  &:focus {
     outline: none;
   }
-  button:active{
-    > span {width: $h2 + 4px;}
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
   }
-  button.checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
+}
+
+
 </style>
