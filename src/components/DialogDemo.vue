a1234d49aa1a64a5b14d4dd9f8005782ef2f5b64
搬砖的女娃子 <1239457176@qq.com>
2022-10-13 16:56:03 +0800

    添加Button组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-        DialogDemo.vue
+        <h1>示例</h1>
+        <Dialog />
     </div>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default{
+    components:{
+        Dialog
+    }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dialog-over">dialog组件</div>
+  <div></div>
+  <div>
+    <header>标题</header>
+    <main>
+      <p>内容111</p>
+      <p>内容222</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>cancle</Button>
+    </footer>
+  </div>
+</template>
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
