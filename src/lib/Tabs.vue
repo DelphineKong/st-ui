732b4212ab77d92edb9b16ae11bad4d8e2b49406
搬砖的女娃子 <1239457176@qq.com>
2022-10-14 17:12:16 +0800

    校验Tabs的子组件是否是tab，防御式编程

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        Tabs
-    </div>
+  <div>Tabs 组件</div>
+  <component :is="defaults[0]"></component>
+  <component :is="defaults[1]"></component>
 </template>
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+  setup(props, context) {
+    console.log({...context.slots.default()[0]})
+    console.log({...context.slots.default()[0]})
+    const defaults=context.slots.default()
+    defaults.forEach((tag)=>{
+        if(tag.type !== Tab){
+            throw Error("Tabs 子标签必须是Tab")
+        }
+    })
+    return{defaults}
+    
+  },
+};
+</script>
