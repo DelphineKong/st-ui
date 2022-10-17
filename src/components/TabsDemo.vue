a091987e1f83ac02788180d08520228c208edd82
搬砖的女娃子 <1239457176@qq.com>
2022-10-17 16:08:28 +0800

    遇到 Vue 3 bug：vuejs/core#2013,插槽内容不更新

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,18 +1,23 @@
 <template>
     <div>Tabs示例</div>
     <h1>示例1</h1>
-    <Tabs>
-        <Tab title="导航1">内容1</Tab>
-        <Tab title="导航2">内容2</Tab>
+    <Tabs v-model:selected="x">
+        <Tab title="导航1">内容1-{{x}}</Tab>
+        <Tab title="导航2">内容2-{{x}}</Tab>
     </Tabs>
 </template>
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import { ref } from 'vue';
 export default{
     components:{
         Tabs,
         Tab
+    },
+    setup() {
+        const x = ref("导航1");
+        return{x}
     }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,32 +1,50 @@
 <template>
   <div class="st-tabs">
     <div class="st-tabs-nav">
-      <div class="st-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+
+      <div class="st-tabs-nav-item" v-for="(t,index) in titles" :key="index"  @click="select(t)" :class="{selected: t=== selected}" >{{t}}</div>
     </div>
+
+
     <div class="st-tabs-content">
       <!-- <component :is="defaults[0]"></component> -->
-      <component class="st-tabs-content-item" v-for="(c,index) in defaults"  :key="index" :is="c"></component>
+      {{current}}
+      <component class="st-tabs-content-item" :is="current"></component>
   </div>
 </div>
   
 </template>
 <script lang="ts">
+import { computed } from 'vue';
 import Tab from './Tab.vue'
 export default {
+  props:{
+    selected:{
+      type:String
+    }
+  },
   setup(props, context) {
-    console.log({...context.slots.default()[0]})
-    console.log({...context.slots.default()[1]})
     const defaults=context.slots.default()
     defaults.forEach((tag)=>{
         if(tag.type !== Tab){
-            throw Error("Tabs 子标签必须是Tab")
+            throw new Error("Tabs 子标签必须是Tab")
         }
     })
+    const current = computed(()=>{
+      console.log("从新return")
+      return defaults.filter((tag)=>{
+        return tag.props.title === props.selected
+      })[0];//取filter返回值的第0个,当前component
+    })
+
     const titles=defaults.map((tag)=>{
       return tag.props.title
-      
     })
-    return{defaults,titles}
+    const select=(title:string)=>{
+      console.log(title)
+      context.emit('update:selected',title)
+    }
+    return{defaults,titles,current,select}
   },
 };
 </script>
