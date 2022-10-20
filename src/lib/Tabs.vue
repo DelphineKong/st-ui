53d3691222def3cc72b36258701a22942c478248
搬砖的女娃子 <1239457176@qq.com>
2022-10-20 14:46:52 +0800

    优化代码2

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,10 +4,15 @@
       <div
         class="st-tabs-nav-item"
         v-for="(t, index) in titles"
-        :ref=" el => { if (t === selected) selectedItem = el; } "
+        :ref="
+          (el) => {
+            if(t === selected) selectedItem = el;
+          }
+        "
         :key="index"
         @click="select(t)"
-        :class="{ selected: t === selected }">
+        :class="{ selected: t === selected }"
+      >
         {{ t }}
       </div>
       <div class="st-tabs-nav-indicator" ref="indicator"></div>
@@ -25,7 +30,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed, onMounted, onUpdated, ref } from "vue";
+import { computed, ref, watchEffect } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -34,33 +39,25 @@ export default {
     },
   },
   setup(props, context) {
-    const selectedItem = ref<HTMLDivElement>(null)
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    const x=()=>{
+    watchEffect(() => {
       // console.log({...navItems.value})
       const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
-      const { left:left1 } = container.value.getBoundingClientRect();
-      const { left:left2 } = selectedItem.value.getBoundingClientRect();
-      const left=left2-left1;
-      indicator.value.style.left=left+'px'
-    }
-     //onMounted只在第一次渲染执行
-    onMounted(x);
-    onUpdated(x);
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error("Tabs 子标签必须是Tab");
       }
     });
-    const current = computed(() => {
-      console.log("从新return");
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected;
-      })[0]; //取filter返回值的第0个,当前component
-    });
+ 
 
     const titles = defaults.map((tag) => {
       return tag.props.title;
@@ -69,7 +66,14 @@ export default {
       console.log(title);
       context.emit("update:selected", title);
     };
-    return { defaults, titles, current, select, indicator ,container,selectedItem};
+    return {
+      defaults,
+      titles,
+      select,
+      indicator,
+      container,
+      selectedItem,
+    };
   },
 };
 </script>
@@ -105,13 +109,13 @@ $border-color: #d9d9d9;
     }
   }
   &-content {
-      padding: 8px 0;
-      &-item {
-        display: none;
-        &.selected {
-          display: block;
-        }
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
       }
     }
+  }
 }
 </style>
