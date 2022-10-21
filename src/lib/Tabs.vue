175a09abc353adaaea40321b611c8b8d14e98145
搬砖的女娃子 <1239457176@qq.com>
2022-10-21 09:00:36 +0800

    watchEffect 单独用的效果是 onMounted 和 onUpdated 一起用的效果一样的 ，但是直接用 watchEffect 会有报错，因为 watchEffect 会在第一次渲染之前就执行，那么第一次渲染之后才有的值都会为空，导致错误。 解决方法就是，先 onMounted ，在 onMounted 里面再 watchEffect 即可；链接https://juejin.cn/post/7007740960910082061

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -42,17 +42,21 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    watchEffect(() => {
-      console.log(selectedItem.value);
-      // console.log({...navItems.value})
-      if(!selectedItem.value){return}
-      const { width } = selectedItem.value.getBoundingClientRect();
-      indicator.value.style.width = width + "px";
-      const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = selectedItem.value.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left + "px";
+    onMounted(() => {
+      watchEffect(() => {
+        console.log(selectedItem.value); // console.log({...navItems.value})
+        if (!selectedItem.value) {
+          return;
+        }
+        const { width } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      });
     });
+
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
