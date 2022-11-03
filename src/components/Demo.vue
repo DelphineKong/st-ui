041af1b325b0a922e600bd5bdd174f5b7740d936
搬砖的女娃子 <1239457176@qq.com>
2022-11-03 17:10:10 +0800

    优化Demo

---
+++ src/components/Demo.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+      <div class="demo">
+        <h2>常规用法</h2>
+        <div class="demo-component">
+          <component :is="component"></component>
+        </div>
+        <div class="demo-actions">
+          <Button @click="codeVisible=!codeVisible">查看代码</Button>
+        </div>
+        <div class="demo-code" v-if="codeVisible">
+          <pre class="language-html" v-html="html"></pre>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script lang="ts">
+    import { computed, ref } from "vue";
+    import Button from '../lib/Button.vue'
+    import  '../../node_modules/prismjs/prism.js'; 
+    import  '../../node_modules/prismjs/themes/prism.css';
+    // import  'prismjs/themes/prism-okaidia.css';
+    const Prism=(window as any).Prism
+    export default{
+        props:{
+            component:Object
+        },
+        setup(props){
+            const html=computed(()=>{
+                return Prism.highlight(props.component.__sourceCode,Prism.languages.html,'html')
+            })
+            const codeVisible=ref(false)
+            return {Button,Prism,html,codeVisible};
+        } 
+    }
+  </script>
+  <style lang="scss" scoped>
+  $border-color: #d9d9d9;
+  .demo {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+    >h2 {
+      font-size: 20px;
+      padding: 8px 16px;
+      border-bottom: 1px solid $border-color;
+    }
+    &-component {
+      padding: 16px;
+    }
+    &-actions {
+      padding: 8px 16px;
+      border-top: 1px dashed $border-color;
+    }
+    &-code {
+      padding: 8px 16px;
+      border-top: 1px dashed $border-color;
+      >pre {
+        line-height: 1.1;
+        font-family: Consolas, 'Courier New', Courier, monospace;
+        margin: 0;
+      }
+    }
+  }
+  
+  </style>
+  
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,47 +1,19 @@
 <template>
   <div>
     <h1>Switch 组件示例</h1>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component">
-        <component :is="Switch1Demo"></component>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
-      </div>
-    </div>
-    <div class="demo">
-      <h2>支持 disabled</h2>
-      <div class="demo-component">
-        <component :is="Switch2Demo"></component>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')"></pre>
-      </div>
-    </div>
+    <Demo :component="Switch1Demo"></Demo>
+    <Demo :component="Switch2Demo"></Demo>
   </div>
 </template>
 <script lang="ts">
-import { ref } from "vue";
-import Switch from "../lib/Switch.vue";
-import Button from '../lib/Button.vue'
+import Demo from "./Demo.vue"
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
-import  '../../node_modules/prismjs/prism.js'; 
-import  '../../node_modules/prismjs/themes/prism.css';
-// import  'prismjs/themes/prism-okaidia.css';
-const Prism=(window as any).Prism
+
 export default {
-  components: { Switch, Button, Switch1Demo, Switch2Demo },
+  components: {  Switch1Demo, Switch2Demo,Demo },
   setup() {
-    const bool = ref(false);
-    return { bool,Switch1Demo,Switch2Demo,Prism};
+    return { Switch1Demo,Switch2Demo};
   },
 };
 </script>
