ecdac0e24190c00df6e071ec1c34a4d3e35e59bd
搬砖的女娃子 <1239457176@qq.com>
2022-11-04 16:34:09 +0800

    修改markdown w

--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,12 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import Markdown from './components/Markdown.vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 import { h } from 'vue'
 const history = createWebHashHistory();
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, {content:string, key: string})
 export const router = createRouter({
     history: history,
     routes: [
@@ -18,15 +21,13 @@ export const router = createRouter({
             component: Doc,
             children: [
                 { path: '', redirect:'/doc/intro' },
-                { path: 'intro', component: md("intro") },
-                { path: 'get-start', component: md("get-started") },
-                { path: 'install', component: md("install") },
+                { path: 'intro', component: md(intro) },
+                { path: 'get-start', component: md(getStarted) },
+                { path: 'install', component: md(install) },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
                 { path: 'tabs', component: TabsDemo },
-
-
             ]
         }
     ]
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import { md } from "./plugins/md";
 import fs from 'fs'
 import {baseParse} from '@vue/compiler-core'
 
+
+
 export default {
+  base: './',
+  assetsDir: 'assets',
   plugins: [md()],
   vueCustomBlockTransforms: {
     demo: (options) => {
