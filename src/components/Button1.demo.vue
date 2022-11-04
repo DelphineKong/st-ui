4103a16aabff9992491144b13d323ce4aacdf1ef
搬砖的女娃子 <1239457176@qq.com>
2022-11-04 08:48:26 +0800

    改造Button Demo

---
+++ src/components/Button1.demo.vue
@@ -0,0 +1,15 @@
+<demo>常规使用</demo>
+
+<template>
+  <div>
+    <Button>hello</Button>
+    <Button theme="link">hello</Button>
+    <Button theme="text">hello</Button>
+  </div>
+</template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
