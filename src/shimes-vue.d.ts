6f95ed0283541fdc9ec6f901b93a5da15acb16fe
搬砖的女娃子 <1239457176@qq.com>
2022-11-14 16:14:13 +0800

    first commit

--- src/shimes-vue.d.ts
+++ src/shimes-vue.d.ts
@@ -4,6 +4,7 @@ declare module '*.vue'{
     const componentOptions:ComponentOptions
     export default componentOptions
 }
+// 帮助ts应该如何理解.md文件
 declare module '*.md'{
     const str:string
     export default str
